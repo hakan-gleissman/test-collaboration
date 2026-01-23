6b1747ca0da478143c2da85a46cabc2e4f0e976b
hakan-gleissman <hakan.gleissman@sprinto.se>
2026-01-23 10:23:59 +0100

    Add additional print statement in getGreeting method

--- src/main/java/se/sprinto/hakan/testcollaboration/TestUtil.java
+++ src/main/java/se/sprinto/hakan/testcollaboration/TestUtil.java
@@ -5,6 +5,7 @@ public class TestUtil {
     public Long getGreeting() {
         System.out.println("Another system out");
         System.out.println("NO");
+        System.out.println("YES");
         return 4L;
     }
 }
