b0ba7bcd3879522067b51971cb751b6e259802e9
hakan-gleissman <hakan.gleissman@sprinto.se>
2026-01-23 09:57:58 +0100

    Add logging to getGreeting method

--- src/main/java/se/sprinto/hakan/testcollaboration/TestUtil.java
+++ src/main/java/se/sprinto/hakan/testcollaboration/TestUtil.java
@@ -3,6 +3,7 @@ package se.sprinto.hakan.testcollaboration;
 public class TestUtil {
 
     public Long getGreeting() {
+        System.out.println("Another system out");
         return 4L;
     }
 }
