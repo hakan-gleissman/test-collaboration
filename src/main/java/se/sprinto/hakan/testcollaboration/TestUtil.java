5ca4d06ea526dd537cbb1567bc35d43f62ea129e
hakan-gleissman <hakan.gleissman@sprinto.se>
2026-01-23 10:25:54 +0100

    Add variable 'a' in getGreeting method

--- src/main/java/se/sprinto/hakan/testcollaboration/TestUtil.java
+++ src/main/java/se/sprinto/hakan/testcollaboration/TestUtil.java
@@ -6,6 +6,7 @@ public class TestUtil {
         System.out.println("Another system out");
         System.out.println("NO");
         System.out.println("YES");
+        int a = 3;
         return 4L;
     }
 }
