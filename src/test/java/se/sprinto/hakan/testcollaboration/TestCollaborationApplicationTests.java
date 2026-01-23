9c48fbec2cafff5c3f7aa9e32a90bc946f76949a
hakan-gleissman <hakan.gleissman@gmail.com>
2026-01-23 09:22:30 +0100

    Initial commit

---
+++ src/test/java/se/sprinto/hakan/testcollaboration/TestCollaborationApplicationTests.java
@@ -0,0 +1,13 @@
+package se.sprinto.hakan.testcollaboration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCollaborationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
