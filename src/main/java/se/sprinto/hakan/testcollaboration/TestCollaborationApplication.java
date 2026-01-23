c9443352b9cb69e6098108392491e2688a67dc09
hakan-gleissman <hakan.gleissman@gmail.com>
2026-01-23 09:23:45 +0100

    TestUtil

--- src/main/java/se/sprinto/hakan/testcollaboration/TestCollaborationApplication.java
+++ src/main/java/se/sprinto/hakan/testcollaboration/TestCollaborationApplication.java
@@ -1,13 +1,18 @@
 package se.sprinto.hakan.testcollaboration;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestCollaborationApplication {
+public class TestCollaborationApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TestCollaborationApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello from TestCollaborationApplication!");
+    }
 }
---
+++ src/main/java/se/sprinto/hakan/testcollaboration/TestUtil.java
@@ -0,0 +1,8 @@
+package se.sprinto.hakan.testcollaboration;
+
+public class TestUtil {
+
+    public String getGreeting() {
+        return "Hello from TestUtil!";
+    }
+}
